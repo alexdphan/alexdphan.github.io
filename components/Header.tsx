d0a4c44dd2544af434160b928dac55d1bfcd33cd
alexdphan <alexphan0515@gmail.com>
2023-08-01 00:24:26 -0400

    fix padding

--- components/Header.tsx
+++ components/Header.tsx
@@ -42,7 +42,7 @@ import ScrambleText from 'components/ScrambleEffect';
 // added a fixed width and height to fill in space before text loads in from animation
 export function Header() {
   return (
-    <header className="flex justify-end p-8 pt-12 pb-0 ">
+    <header className="flex justify-end p-8 pt-6 pb-0 ">
       <Button variant="link" className="link-with-animation">
         <div style={{ width: '40px', height: '15px' }}>
           <ScrambleText text="Home" delay={3.25} />
