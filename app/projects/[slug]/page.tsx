59dfe7ab2bb29d670e0a55b905ad5b3bde688abd
alexdphan <alexphan0515@gmail.com>
2023-08-03 04:07:28 -0400

    encode/decode

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -4,26 +4,23 @@ import { getMDXComponent } from 'next-contentlayer/hooks';
 import { Mdx } from 'components/mdx';
 
 export const generateStaticParams = async () =>
-  allProjects.map((project) => ({ slug: project._raw.flattenedPath }));
+  allProjects.map((project) => ({
+    slug: encodeURIComponent(project._raw.flattenedPath),
+  }));
 
 export const generateMetadata = ({ params }) => {
+  const decodedSlug = decodeURIComponent(params.slug);
   const project = allProjects.find(
-    // (project) => project._raw.flattenedPath === `projects/${params.slug}`
-    (project) => project._raw.flattenedPath === `projects/${params.slug}`
+    (project) => project._raw.flattenedPath === `projects/${decodedSlug}`
   );
 
   return { title: project.title };
 };
 
 const ProjectLayout = ({ params }: { params: { slug: string } }) => {
-  // changed the params.slug to `posts/${params.slug}` to match the flattenedPath
-
-  // console.log('Parameters: ', params);
-  // console.log('All projects: ', allProjects);
-
+  const decodedSlug = decodeURIComponent(params.slug);
   const project = allProjects.find(
-    // (project) => project._raw.flattenedPath === `projects/${params.slug}`
-    (project) => project._raw.flattenedPath === `projects/${params.slug}`
+    (project) => project._raw.flattenedPath === `projects/${decodedSlug}`
   );
 
   return (
