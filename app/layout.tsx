04977bac7911caf3eac575dde34acdcf36be1c33
alexdphan <alexphan0515@gmail.com>
2023-08-04 21:18:09 -0400

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Footer } from '../components/Footer';
 
 import { Roboto_Mono } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script';
 
 // this is so latex works properly
 import 'katex/dist/katex.min.css'; // put the path to your katex.min.css file here
@@ -25,6 +26,18 @@ export default function RootLayout({ children }) {
         <title>Alex Phan</title>
         {/* Add icon here */}
         <link rel="icon" type="image/x-icon" href="/images/AP.png" />
+        <div className="container">
+          <Script src="https://www.googletagmanager.com/gtag/js?id=G-NG9MP9KV8W" />
+          <Script id="google-analytics">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-NG9MP9KV8W');
+        `}
+          </Script>
+        </div>
       </head>
       <body className="flex flex-col items-center justify-between min-h-screen">
         <div className="flex flex-col w-full max-w-xl min-h-screen">
